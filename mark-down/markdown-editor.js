2e4804cd376dc7874421e3a67f55b3f62d0bd310
Thanh Ly <hien689@gmail.com>
2023-09-11 14:45:00 -1000

    markdown got preview to work

--- mark-down/markdown-editor.js
+++ mark-down/markdown-editor.js
@@ -1,15 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const editor = document.getElementById('editor');
-    const preview = document.getElementById('preview');
+const markdownInput = document.getElementById('markdown-input');
+const markdownOutput = document.getElementById('markdown-output');
 
-    // Initial render
-    renderMarkdown();
-
-    editor.addEventListener('input', renderMarkdown);
-
-    function renderMarkdown() {
-        const markdownText = editor.value;
-        const htmlOutput = marked(markdownText); // Using the marked library to convert markdown to HTML
-        preview.innerHTML = htmlOutput;
-    }
+markdownInput.addEventListener('input', function() {
+    const markdownText = markdownInput.value;
+    const html = marked(markdownText);
+    markdownOutput.innerHTML = html;
 });
