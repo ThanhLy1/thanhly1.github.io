1307faebd74576f640a62ab32768c020a87bffd1
Thanh Ly <hien689@gmail.com>
2023-09-11 16:13:27 -1000

    still working on markdown

--- mark-down/markdown-editor.js
+++
@@ -1,8 +0,0 @@
-const markdownInput = document.getElementById('markdown-input');
-const markdownOutput = document.getElementById('markdown-output');
-
-markdownInput.addEventListener('input', function() {
-    const markdownText = markdownInput.value;
-    const html = marked(markdownText);
-    markdownOutput.innerHTML = html;
-});
