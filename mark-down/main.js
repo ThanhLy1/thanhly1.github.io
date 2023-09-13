bf978103763c44f54ab6dfe4f6e65119777a9016
Thanh Ly <you@example.comhien689@gmail.com>
2023-09-12 23:37:17 -1000

    fixing preview

--- mark-down/main.js
+++ mark-down/main.js
@@ -1,7 +1,3 @@
-    
-    // Create an instance of the Marked class
-    const markedInstance = new marked.Marked();
-
     // Enable GFM and other related options
     markedInstance.setOptions({
         gfm: true,               // Enable GitHub Flavored Markdown
@@ -14,59 +10,54 @@
         xhtml: false             // Don't output self-closing tags
     });
     
+    // Create an instance of the Marked class
+    const markedInstance = new marked.Marked();
+
     // Use the GFM extension
     if (window.markedGfmHeadingId) {
         markedInstance.use(window.markedGfmHeadingId);
     }
-        
-        // Create an instance of the Marked class
-        //const markedInstance = new marked.Marked();
-    
-        // Use the GFM extension
-        if (window.markedGfmHeadingId) {
-            markedInstance.use(window.markedGfmHeadingId);
-        }
-    
-        // Get the editor and preview elements
-        const editor = document.getElementById('editor');
-        const preview = document.getElementById('preview');
-    
-        // Store the last edited markdown
-        let lastEditedMarkdown = "";
-    
-        // Function to render markdown to HTML
-        function renderMarkdown() {
-            const markdownText = editor.value;
-            const html = markedInstance.parse(markdownText);
-            preview.innerHTML = '<div class="markdown-body">' + html + '</div>';
-        }
-    
-        // Load reference.md content
-        function loadReference() {
-            fetch('reference.md')
-                .then(response => response.text())
-                .then(data => {
-                    editor.value = data;
-                    renderMarkdown();
-                });
-        }
-    
-        // Handle dropdown changes
-        document.getElementById('modeSelector').addEventListener('change', function() {
-            if (this.value === 'reference') {
-                // Store the current markdown
-                lastEditedMarkdown = editor.value;
-                // Load the reference.md content
-                loadReference();
-            } else if (this.value === 'edit') {
-                // Load the last edited markdown
-                editor.value = lastEditedMarkdown;
+
+    // Get the editor and preview elements
+    const editor = document.getElementById('editor');
+    const preview = document.getElementById('preview');
+
+    // Store the last edited markdown
+    let lastEditedMarkdown = "";
+
+    // Function to render markdown to HTML
+    function renderMarkdown() {
+        const markdownText = editor.value;
+        const html = markedInstance.parse(markdownText);
+        preview.innerHTML = '<div class="markdown-body">' + html + '</div>';
+    }
+
+    // Load reference.md content
+    function loadReference() {
+        fetch('reference.md')
+            .then(response => response.text())
+            .then(data => {
+                editor.value = data;
                 renderMarkdown();
-            }
-        });
-    
-        // Add an event listener to the editor
-        editor.addEventListener('input', renderMarkdown);
-    
-        // Initial render
-        renderMarkdown();
+            });
+    }
+
+    // Handle dropdown changes
+    document.getElementById('modeSelector').addEventListener('change', function() {
+        if (this.value === 'reference') {
+            // Store the current markdown
+            lastEditedMarkdown = editor.value;
+            // Load the reference.md content
+            loadReference();
+        } else if (this.value === 'edit') {
+            // Load the last edited markdown
+            editor.value = lastEditedMarkdown;
+            renderMarkdown();
+        }
+    });
+
+    // Add an event listener to the editor
+    editor.addEventListener('input', renderMarkdown);
+
+    // Initial render
+    renderMarkdown();
