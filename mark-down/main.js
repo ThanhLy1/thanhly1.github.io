9a959c1b37e65897933870ac5f33df8c0f04bda5
Thanh Ly <you@example.comhien689@gmail.com>
2023-09-13 02:02:35 -1000

    Added Highlight.js for code block

--- mark-down/main.js
+++ mark-down/main.js
@@ -1,9 +1,12 @@
-    
-    // Create an instance of the Marked class
+// Create an instance of the Marked class
 const markedInstance = new marked.Marked();
 
-// Enable GFM and other related options
+// Configure markedInstance to use highlight.js
 markedInstance.setOptions({
+    highlight: function(code, language) {
+        const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
+        return hljs.highlight(validLanguage, code).value;
+    },
     gfm: true,               // Enable GitHub Flavored Markdown
     breaks: true,            // Use GFM line breaks
     headerIds: true,         // Use GFM header IDs
@@ -18,55 +21,64 @@ markedInstance.setOptions({
 if (window.markedGfmHeadingId) {
     markedInstance.use(window.markedGfmHeadingId);
 }
-    
-    // Create an instance of the Marked class
-    //const markedInstance = new marked.Marked();
 
-    // Use the GFM extension
-    if (window.markedGfmHeadingId) {
-        markedInstance.use(window.markedGfmHeadingId);
-    }
+// Get the editor and preview elements
+const editor = document.getElementById('editor');
+const preview = document.getElementById('preview');
 
-    // Get the editor and preview elements
-    const editor = document.getElementById('editor');
-    const preview = document.getElementById('preview');
+// Store the last edited markdown
+let lastEditedMarkdown = "";
 
-    // Store the last edited markdown
-    let lastEditedMarkdown = "";
+// Function to render markdown to HTML
+function renderMarkdown() {
+    const markdownText = editor.value;
+    const html = markedInstance.parse(markdownText);
+    preview.innerHTML = '<div class="markdown-body">' + html + '</div>';
 
-    // Function to render markdown to HTML
-    function renderMarkdown() {
-        const markdownText = editor.value;
-        const html = markedInstance.parse(markdownText);
-        preview.innerHTML = '<div class="markdown-body">' + html + '</div>';
-    }
+    // Highlight code blocks
+    document.querySelectorAll('pre code').forEach((block) => {
+        hljs.highlightBlock(block);
+    });
+}
 
-    // Load reference.md content
-    function loadReference() {
-        fetch('reference.md')
-            .then(response => response.text())
-            .then(data => {
-                editor.value = data;
-                renderMarkdown();
-            });
-    }
+// Load reference.md content
+function loadReference(filename) {
+    fetch(filename)
+        .then(response => response.text())
+        .then(data => {
+            editor.value = data;
+            renderMarkdown();
+        });
+}
 
-    // Handle dropdown changes
-    document.getElementById('modeSelector').addEventListener('change', function() {
-        if (this.value === 'reference') {
-            // Store the current markdown
-            lastEditedMarkdown = editor.value;
+
+// Handle dropdown changes
+document.getElementById('modeSelector').addEventListener('change', function() {
+    switch (this.value) {
+        case 'reference':
             // Load the reference.md content
-            loadReference();
-        } else if (this.value === 'edit') {
+            loadReference('reference.md');
+            break;
+        case 'basicReference':
+            // Load the basicReference.md content
+            loadReference('basicReference.md');
+            break;
+        case 'advancedReference':
+            // Load the advancedReference.md content
+            loadReference('advancedReference.md');
+            break;
+        case 'edit':
+        default:
             // Load the last edited markdown
             editor.value = lastEditedMarkdown;
             renderMarkdown();
-        }
-    });
+            break;
+    }
+});
 
-    // Add an event listener to the editor
-    editor.addEventListener('input', renderMarkdown);
 
-    // Initial render
-    renderMarkdown();
+// Add an event listener to the editor
+editor.addEventListener('input', renderMarkdown);
+
+// Initial render
+renderMarkdown();
