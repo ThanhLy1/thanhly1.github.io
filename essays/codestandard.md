3d25cbfb4a1367698843b6aae0a778c82c371d86
Thanh Ly <hien689@gmail.com>
2023-09-17 18:21:36 -1000

    fix markdown, added code standard essay

---
+++ essays/codestandard.md
@@ -0,0 +1,37 @@
+---
+layout: essay
+type: essay
+title: In Defense of Coding Standards: Beyond Aesthetics and into Understanding
+date: 2023-09-17
+labels:
+  - Coding Standards
+  - Software Engineering
+  - ESLint
+  - IntelliJ
+---
+
+![CodeStandard](../img/codestandard/codestandard.png)
+
+## Introduction
+
+In a world rife with digital information, coding has rapidly transformed from an obscure hobby to a mainstream professional skill. Yet, as more people dip their toes into the vast ocean of programming, the importance of establishing consistency has never been clearer. Enter: coding standards.
+
+Many beginners often misconstrue coding standards as mere punctilious preferences — a dispute between tabs and spaces, or the placement of a curly brace. But as we delve deeper into the realm of software engineering, we recognize that coding standards transcend aesthetic appeal; they form the bedrock of efficient, readable, and maintainable code.
+
+To borrow a perspective, think of code as a written essay. Without consistent punctuation, grammar, and structure, even the most profound thoughts become obscured, and the message gets lost amidst the chaos. Just as writers use style guides to lend uniformity and clarity to their prose, developers use coding standards to ensure that their code, which could be read by countless other developers, is universally understandable.
+
+## ESLint and IntelliJ: A Tale of Trials
+
+This brings me to ESLint, a static code analysis tool for identifying and rectifying problematic patterns in JavaScript code. My journey with ESLint in the IntelliJ environment was, to put it mildly, tumultuous. The initial setup felt like navigating a labyrinth, with multiple iterations of the same steps leaving me frustrated. Perhaps it was the switch from a comfortable WSL terminal within Visual Studio Code, where plugins effortlessly maintained code cleanliness. Nevertheless, the repeated process in IntelliJ eventually became more streamlined, thanks to an automation script.
+
+Despite the initial hurdles, I echo the sentiment that coding standards, facilitated by tools like ESLint, play an instrumental role in learning a programming language. By pointing out inconsistencies and potential errors, these standards serve as strict yet enlightening mentors, guiding novices from the pitfalls of amateur coding to the clarity of professional programming.
+
+## Beauty in Diversity
+
+It's worth noting, though, that coding standards are not about enforcing a singular, monolithic style. Indeed, there's a spectrum of 'prettiness', and what's essential is finding a balance — a standard that a majority can resonate with and say, "Yes, this makes sense."
+
+Veteran engineers, with years of trial and error behind them, have gravitated towards certain standards, whether molded by organizational protocols or personal preferences. For newcomers, these standards might seem arbitrary or even cumbersome. But there's wisdom in that consistency. With tools like ESLint, the transition might feel like a rite of passage – a molding process, transforming one's coding habits from raw, unrefined patterns to structured, professional methodologies.
+
+## Conclusion
+
+In conclusion, while the initial transition to uphold coding standards can be daunting, it is a transformative journey. Beyond the superficial debates about tabs or spaces, there lies the core principle: code is a collaborative effort. By adhering to shared standards, we ensure that our digital tapestry, woven with countless lines of code, remains coherent, elegant, and timeless.
--- mark-down/main.js
+++ mark-down/main.js
@@ -7,17 +7,17 @@ markedInstance.setOptions({
         const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
         return hljs.highlight(validLanguage, code).value;
     },
-    gfm: true,               // Enable GitHub Flavored Markdown
-    breaks: true,            // Use GFM line breaks
-    headerIds: true,         // Use GFM header IDs
-    mangle: true,            // Encode code blocks
-    pedantic: false,         // Don't be strict about parsing GFM
-    smartLists: true,        // Use smarter list behavior than the original markdown
-    smartypants: false,      // Don't use "smart" typographic punctuation
-    xhtml: false             // Don't output self-closing tags
+    gfm: true,        // Enable GitHub Flavored Markdown
+    breaks: true,     // Use GFM line breaks
+    headerIds: true,  // Use GFM header IDs
+    mangle: true,     // Encode code blocks
+    pedantic: false,  // Don't be strict about parsing GFM
+    smartLists: true, // Use smarter list behavior than the original markdown
+    smartypants: false, // Don't use "smart" typographic punctuation
+    xhtml: false      // Don't output self-closing tags
 });
 
-// Use the GFM extension
+// Use the GFM extension if available
 if (window.markedGfmHeadingId) {
     markedInstance.use(window.markedGfmHeadingId);
 }
@@ -29,19 +29,25 @@ const preview = document.getElementById('preview');
 // Store the last edited markdown
 let lastEditedMarkdown = "";
 
+// Store the last edit made in "Edit" container
+let lastEditInEditMode = "";
+
 // Function to render markdown to HTML
 function renderMarkdown() {
     const markdownText = editor.value;
+    if (markdownText !== lastEditedMarkdown) {
+        lastEditedMarkdown = markdownText;
+    }
     const html = markedInstance.parse(markdownText);
     preview.innerHTML = '<div class="markdown-body">' + html + '</div>';
 
     // Highlight code blocks
     document.querySelectorAll('pre code').forEach((block) => {
-        hljs.highlightBlock(block);
+        hljs.highlightElement(block);
     });
 }
 
-// Load reference.md content
+// Function to load reference.md content
 function loadReference(filename) {
     fetch(filename)
         .then(response => response.text())
@@ -51,33 +57,39 @@ function loadReference(filename) {
         });
 }
 
+// Store the last selected mode in local storage
+let lastSelectedMode = localStorage.getItem('selectedMode') || 'edit';
+
 // Handle dropdown changes
-document.getElementById('modeSelector').addEventListener('change', function() {
-    switch (this.value) {
-        case 'reference':
-            // Load the reference.md content
-            loadReference('reference.md');
-            break;
-        case 'basicReference':
-            // Load the basicReference.md content
-            loadReference('basicReference.md');
-            break;
-        case 'advancedReference':
-            // Load the advancedReference.md content
-            loadReference('advancedReference.md');
-            break;
-        case 'edit':
-        default:
-            // Load the last edited markdown
-            editor.value = lastEditedMarkdown;
-            renderMarkdown();
-            break;
+document.getElementById('modeSelector').addEventListener('change', function () {
+    console.log("Dropdown changed to:", this.value);
+    
+    if (this.value === 'reference') {
+        // Store the last edit made in "Edit" mode
+        if (lastSelectedMode === 'edit') {
+            lastEditInEditMode = editor.value;
+        }
+
+        // Load the reference content
+        loadReference('reference.md');
+        lastSelectedMode = 'reference'; // Update the last selected mode
+        localStorage.setItem('selectedMode', 'reference'); // Store in local storage
+    } else { // Default to 'edit' mode
+        console.log("Last edited content:", lastEditInEditMode);
+        
+        // Restore the last edit made in "Edit" mode
+        if (lastEditInEditMode) {
+            editor.value = lastEditInEditMode;
+        }
+        renderMarkdown();
+        lastSelectedMode = 'edit'; // Update the last selected mode
+        localStorage.setItem('selectedMode', 'edit'); // Store in local storage
     }
 });
-
 
 // Add an event listener to the editor
 editor.addEventListener('input', renderMarkdown);
 
-// Initial render
-renderMarkdown();
+// When the page loads, select the last selected mode
+document.getElementById('modeSelector').value = lastSelectedMode;
+renderMarkdown(); // Initial render based on the mode selected
